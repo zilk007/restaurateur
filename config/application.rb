0341f290f61bcb4be1638f2bf4c4151b1a8a787a
Alan <alan.t.cumiskey@gmail.com>
2015-08-19 01:38:25 +0100

    Your Commit

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Restaurateur
   class Application < Rails::Application
     #config.assets.enabled = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
     #config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset sevrer (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
 
   # Compress JavaScripts and CSS.
